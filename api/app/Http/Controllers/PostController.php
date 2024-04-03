46b804285f7c2768814597821ceb92beb8989c8e
SONOMI\yamau <yamauchi.unt@gmail.com>
2024-04-03 11:41:43 +0900

    投稿/詳細処理1/5実装

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
-    //
+    //投稿
+    public function store(StorePostRequest $request)
+    {
+        // バリデーション済データ取得
+        $validated = $request->validated();
+
+        $storedPostId = null;
+
+        DB::transaction(function () use ($validated, &$storedPostId) {
+            // 投稿テーブルへ登録
+            $post = Post::store($validated);
+            $storedPostId = $post->id;
+
+            // 画像ファイル名を変更しパス取得
+            $imagePath = "images/{$storedPostId}.jpeg";
+
+            // デコードされた画像データをストレージに保存
+            Storage::put($imagePath, $validated['decoded_image']);
+        });
+
+        return response()->json(['post_id' => $storedPostId], 201);
+    }
 }
