ab04532f25ab43d7891b976060be11c569df7e4d
SONOMI\yamau <yamauchi.unt@gmail.com>
2024-04-03 15:06:59 +0900

    コメント送信/詳細処理1/4実装

---
+++ api/app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'comment' => [
+                'required',
+                'max:255',
+            ],
+        ];
+    }
+
+    /**
+     * エラーメッセージのカスタマイズ
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'comment.required' => ':attributeを入力してください。',
+            'comment.max'=> ':attributeは255文字以内で入力してください。',
+        ];
+    }
+
+    /**
+     * バリデーション属性のカスタマイズ
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'comment' => 'コメント',
+        ];
+    }
+}
