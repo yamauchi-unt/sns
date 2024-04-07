be642120c566667953ee252ff74ed12c24adad18
SONOMI\yamau <yamauchi.unt@gmail.com>
2024-04-07 15:48:30 +0900

    クラス名変更

--- api/app/Rules/ImageFileType.php
+++ api/app/Rules/ImageFileType.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class Base64FileType implements Rule
+class ImageFileType implements Rule
 {
     protected $allowedMimeType;
 
--- api/app/Rules/ImageMaxSize.php
+++ api/app/Rules/ImageMaxSize.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class Base64MaxSize implements Rule
+class ImageMaxSize implements Rule
 {
     protected $maxSize;
 
