50bc729e48f25463bf9ac0bf95bc23abb9a28e29
SONOMI\yamau <yamauchi.unt@gmail.com>
2024-04-15 10:54:54 +0900

    テストコード・トークン削除の機能テスト作成

--- api/tests/Feature/AuthTokenDestroyTest.php
+++ api/tests/Feature/AuthTokenDestroyTest.php
@@ -9,7 +9,9 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
-// トークン削除 機能テスト
+/**
+ * トークン削除 機能テスト
+ */
 class AuthTokenDestroyTest extends TestCase
 {
     use RefreshDatabase;
@@ -17,61 +19,83 @@ class AuthTokenDestroyTest extends TestCase
     protected $user;
     protected $token;
     protected $tokenId;
+    protected $headers;
 
-    // テスト実行前に登録済ユーザ・トークン生成
+    /**
+     * URL
+     */
+    protected $path = '/api/auth/token';
+
+    /**
+     * 各テストメソッド実行前に、テスト用ユーザ・トークン・リクエストヘッダー作成
+     */
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->user = User::factory()->create();
         $this->token = $this->user->createToken('test-token')->plainTextToken;
         $this->tokenId = explode('|', $this->token, 2)[0];
+        $this->headers = [
+            'Authorization' => 'Bearer ' . $this->token,
+            'Accept' => 'application/json',
+        ];
     }
 
-    // 有効なトークンで削除成功
-    public function test_destroy_with_token_valid_return_204(): void
+    /**
+     * 有効なリクエスト（正常）
+     */
+    public function test_destroy_with_valid_request_return_204(): void
     {
         // Act
-        $response = $this->withHeaders(['Authorization' => 'Bearer '. $this->token])
-                         ->delete('/api/auth/token');
+        $response = $this->withHeaders($this->headers)->delete($this->path);
         // Assert
         $response->assertStatus(204);
-        $this->assertDatabaseMissing('personal_access_tokens', [
-            'id' => $this->tokenId,
-        ]);
+        $this->assertDatabaseMissing('personal_access_tokens', ['id' => $this->tokenId]);
     }
 
-    // Authorizationヘッダー無で削除失敗
-    public function test_destroy_without_authorization_header_return_400(): void
+    /**
+     * Authorization無でリクエスト（異常）
+     */
+    public function test_destroy_without_authorization_return_401(): void
     {
         // Act
-        $response = $this->post('/api/auth/token');
+        $response = $this->withHeaders(['Accept' => 'application/json'])
+                         ->delete($this->path);
         // Assert
-        $response->assertStatus(400);
-        $this->assertDatabaseHas('personal_access_tokens', [
-            'id' => $this->tokenId,
-        ]);
+        $response->assertStatus(401);
+        $this->assertDatabaseHas('personal_access_tokens', ['id' => $this->tokenId]);
     }
 
-    // 有効期限切れトークンで削除失敗
-    public function test_destroy_with_token_invalid_return_401(): void
+    /**
+     * 不正なトークンでリクエスト（異常）
+     */
+    public function test_destroy_with_invalid_token_return_401(): void
     {
         // Arrange
-        $user = User::factory()->create([
-            'user_id' => 'Invalid_User'
-        ]);
-        $token = $user->createToken('test-token')->plainTextToken;
-        $tokenId = explode('|', $token, 2)[0];
-        DB::table('personal_access_tokens')->where('id', $tokenId)->update([
+        $invalidToken = 'intalid_token';
+        // Act
+        $response = $this->withHeaders(['Authorization' => 'Bearer ' . $invalidToken,
+                                        'Accept'        => 'application/json'])
+                         ->delete($this->path);
+        // Assert
+        $response->assertStatus(401);
+        $this->assertDatabaseHas('personal_access_tokens', ['id' => $this->tokenId]);
+    }
+
+    /**
+     * 有効期限切れトークンでリクエスト（異常）
+     */
+    public function test_destroy_with_expired_token_return_401(): void
+    {
+        // Arrange
+        DB::table('personal_access_tokens')->where('id', $this->tokenId)->update([
             'created_at' => Carbon::now()->subDays(1)
         ]);
         // Act
-        $response = $this->withHeaders(['Authorization' => 'Bearer '. $token,
-                                        'Accept'        => 'application/json'])
-                         ->delete('/api/auth/token');
+        $response = $this->withHeaders($this->headers)->delete($this->path);
         // Assert
         $response->assertStatus(401);
-        $this->assertDatabaseHas('personal_access_tokens', [
-            'id' => $tokenId,
-        ]);
+        $this->assertDatabaseHas('personal_access_tokens', ['id' => $this->tokenId]);
     }
 }
